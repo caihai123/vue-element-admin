47f12487b551a37a1331430d53c0aa8b0798264e
caihaibjb <1027747358@qq.com>
2019-12-22 20:23:18 +0800

    优化二级菜单长度为1确是个目录的bug

--- src/layout/SideMenu/sideBar.vue
+++ src/layout/SideMenu/sideBar.vue
@@ -1,24 +1,18 @@
 // 递归组件，用于渲染导航菜单
 <template>
   <fragment v-if="!item.hidden">
-    <el-submenu v-if="item.children&&item.children.length>1" :index="item.path">
-      <template slot="title">
-        <Item :icon="item.meta.icon" :title="item.meta.title"></Item>
-      </template>
-      <side-bar v-for="route in item.children" :key="route.path" :item="route"></side-bar>
-    </el-submenu>
+    <template v-if="item.children">
+      <el-submenu v-if="item.children.length>1" :index="item.path">
+        <template slot="title">
+          <Item :icon="item.meta.icon" :title="item.meta.title"></Item>
+        </template>
+        <side-bar v-for="route in item.children" :key="route.path" :item="route"></side-bar>
+      </el-submenu>
 
-    <div
-      class="router-link"
-      v-else-if="item.children&&item.children.length==1"
-      @click="router(item.redirect||item.path)"
-    >
-      <el-menu-item :index="item.children[0].path">
-        <Item :icon="item.children[0].meta.icon" :title="item.children[0].meta.title"></Item>
-      </el-menu-item>
-    </div>
+      <side-bar v-else :key="item.children[0].path" :item="item.children[0]"></side-bar>
+    </template>
 
-    <div class="router-link" v-else @click="router(item.redirect||item.path)">
+    <div v-else class="router-link" @click="router(item.redirect||item.path)">
       <el-menu-item :index="item.path">
         <Item :icon="item.meta.icon" :title="item.meta.title"></Item>
       </el-menu-item>
