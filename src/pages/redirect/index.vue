8e8dde342dc9b3aea94b8f5fc70136c9a2551217
caihaibjb <1027747358@qq.com>
2019-12-28 19:41:50 +0800

    处理了刷新之后再也不能缓存的问题

--- src/pages/redirect/index.vue
+++ src/pages/redirect/index.vue
@@ -1,9 +1,15 @@
 //用于路由刷新，将会重定向到指定路由
 <script>
+import store from "@/layout/store";
 export default {
   beforeCreate() {
     const { params, query } = this.$route;
     const { path } = params;
+    store.tabsItem.forEach(item => {
+      if (item.to === "/" + path && item.name) {
+        store.addInclude(item.name);
+      }
+    });
     this.$router.replace({ path: "/" + path, query });
   },
   render: function(h) {
